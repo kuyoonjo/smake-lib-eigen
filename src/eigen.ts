5795766e11be6a6b140540f7fceb63b6c7531f21
Yu Chen <yu.chen@live.ie>
2021-08-03 15:21:59 +0800

    bugfix sep.

--- src/eigen.ts
+++ src/eigen.ts
@@ -1,11 +1,11 @@
-import { sep, resolve } from 'path';
+import { resolve } from 'path';
 import { LLVM } from 'smake';
 
 export function eigen(t: LLVM) {
   Object.defineProperty(t, 'sysIncludedirs', {
     value: [
       ...t.sysIncludedirs,
-      resolve(__dirname, '..', 'eigen').replace(new RegExp(sep, 'g'), '/'),
+      resolve(__dirname, '..', 'eigen').replace(/\\/g, '/'),
     ],
     configurable: true,
   });
